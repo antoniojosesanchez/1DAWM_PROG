4eca89ade4f981bc8bc2d41176c4f171bcdf2526
Antonio J. Sánchez <ajsb.ies@gmail.com>
2024-03-20 13:00:52 +0100

    Add files via upload

--- ejercicios-repaso/Ejercicio12.java
+++ ejercicios-repaso/Ejercicio12.java
@@ -18,12 +18,13 @@ public class Ejercicio12 {
         int fil ;
         int col ;
         int turno = 1 ;
+        int fichas = SIZE * SIZE ;
         int[][] tablero = new int[SIZE][SIZE] ;
-        
+        dibujarTablero(tablero) ; 
 
         do {
 
-            dibujarTablero(tablero) ; 
+            
             System.out.println("\n\nTURNO JUGADOR " + turno) ;
             System.out.print("Introduzca las coordenadas, por ejemplo b2: ") ;
             coord = System.console().readLine() ;
@@ -32,15 +33,65 @@ public class Ejercicio12 {
             fil = coord.charAt(0) - 97 ;
             col = coord.charAt(1) - 48 - 1 ;
 
-            // colocamos la ficha
-            tablero[fil][col] = turno ;
+            // colocamos la ficha siempre y cuando la celda no esté ocupada
+            if (tablero[fil][col]==0) {
 
-            // cambiamos el turno
-            turno = (turno==1)?2:1 ; 
+                // colocamos la ficha
+                tablero[fil][col] = turno ;
+
+                // resto ficha
+                fichas-- ;
+
+                // comprobamos si hay tres en raya
+                if ((comprobarHorizontal(tablero, fil, turno)) ||
+                    (comprobarVertical(tablero, col, turno))) {
+
+                    System.out.println("Enhorabuena jugador " + turno + ". ¡Has ganado!");
+                    fin = true ;
+                } else if(fichas == 0) {
+                    System.out.println("El juego termina en empate.");
+                    fin = true ;                
+                }
+                
+
+                // cambiamos el turno
+                turno = (turno==1)?2:1 ; 
+            } else {
+                System.out.println("La celda está ocupada.\n") ;
+            }
+
+            dibujarTablero(tablero) ; 
             
         } while(!fin) ;
 
     }
+
+    /**     
+     * @param tablero
+     * @param fil
+     * @return
+     */
+    private static boolean comprobarHorizontal(int[][] tablero, int fil, int turno) {
+
+        int i = 0 ;        
+
+        while ((i < SIZE) && (tablero[fil][i]==turno)) { i++ ; }
+        return (i >= SIZE) ;
+    }
+
+    /**     
+     * @param tablero
+     * @param col
+     * @return
+     */
+    private static boolean comprobarVertical(int[][] tablero, int col, int turno) {
+
+        int i = 0 ;        
+
+        while ((i < SIZE) && (tablero[i][col]==turno)) { i++ ; }
+        return (i >= SIZE) ;
+    }
+
 
     /**     
      * @param cadena
