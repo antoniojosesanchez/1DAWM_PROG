a9d002486ff797ed2e90d47fd760c3bb837f6371
Antonio J. Sánchez <asanbuj282@g.educaand.es>
2024-04-11 13:56:19 +0200

    Ejercicios de Colecciones y Diccionarios

--- tema10/Ejercicio03.java
+++ tema10/Ejercicio03.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 /**
  * Escribe un programa que ordene 10 números enteros introducidos por teclado y almacenados en 
  * un objeto de la clase ArrayList.
@@ -7,9 +11,28 @@
  */
 
 public class Ejercicio03 {
+
+    private static final int TAMANO = 10 ;
    
     public static void main(String[] args) {
         
-    }
+        int i ;
+        int num ;
+        ArrayList<Integer> numeros = new ArrayList<Integer>() ;
 
+        for(i=0; i<TAMANO; i++) {
+            System.out.print("Introduce un número: ") ;
+            num = Integer.parseInt(System.console().readLine()) ;
+
+            numeros.add(num) ; 
+        }
+
+        // ordenamos la colección de números
+        Collections.sort(numeros) ;
+
+        // mostramos
+        for(int item: numeros) {
+            System.out.println(item) ;
+        }
+    }
 }
